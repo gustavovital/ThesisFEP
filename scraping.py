66fd48118f562a4a7735588e29b8553a74834e4d
Gustavo de Oliveira <gustavovital@id.uff.br>
2021-11-22 18:29:40 +0000

    get dates and refs

--- scraping.py
+++ scraping.py
@@ -12,50 +12,55 @@ from bs4 import BeautifulSoup
 from time import sleep
 # import pandas as pd
 
+# URL of press conference
 URL = "https://www.ecb.europa.eu/press/pressconf/html/index.en.html"
 
+# Initialize Driver
 driver = webdriver.Chrome()
 driver.get(URL)
 
 driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
 last_height = driver.execute_script("return document.body.scrollHeight")
-sleep(1)
 
-#
-# driver.execute_script("window.scrollTo(0, 1080.49)")
-# sleep(2)
-# driver.execute_script("window.scrollTo(0, 2160)")
-# sleep(10)
+# Wait til load the page
+sleep(0.5)
 
+# Generalise for the full page
 length = 1080
-
-
 while (length < last_height):
     driver.execute_script(f'window.scrollTo(0, {length})')
-    sleep(1)
+    sleep(0.5)
     length += 1080
     last_height = driver.execute_script("return document.body.scrollHeight")
 
-# while driver.find_element(By.TAG_NAME, 'div'):
-#     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-#     Divs=driver.find_element(By.TAG_NAME, 'div').text
-#     if 'End of Results' in Divs:
-#         print('end')
-#         break
-#     else:
-#         print(Divs)
-#         continue
-
+# Selenium to Bs4
 main = BeautifulSoup(driver.page_source, 'html.parser')
-# print(main.prettify())
 
+# Get the page component
 body = main.find('body', attrs='project-ecb section-press sub-pressconf -top-notice')
 div = body.find('div', attrs={'id': 'main-wrapper'})
 div_main = div.find('main')
 div_div = div_main.find('div', attrs={'class': 'definition-list -filter'})
 dl = div_div.find('dl', attrs={'id': 'lazyload-container'})
 dl_div = dl.findAll('div', attrs={'class': 'lazy-load loaded'})
-print('=========== PRINT DIV ==============')
+
 print(dl_div)
-# print(div)
-# print(dl_div.find('dt'))
+# print(dl_div.prettify)
+# print(dl_div.findAll('dt')[0])
+# print(driver.find_element_by_tag_name('dt'))
+
+
+# for date in dl_div.findAll('dt'):
+#     print('Date: ' + date['isodate'])
+#
+# for ref in dl_div.findAll('dd'):
+#     print('href: ' + ref.find('a')['href'])
+
+
+# print(dl_div[1])
+
+for tags in dl_div:
+    for date in tags.findAll('dt'):
+        print('Date: ', date['isodate'])
+    for ref in tags.findAll('dd'):
+        print('Href: ', ref.find('a')['href'])
