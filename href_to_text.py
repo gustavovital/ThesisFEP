6ce3428014c22f8b0fc11bd588c936a47359a61b
Gustavo de Oliveira <gustavovital@id.uff.br>
2021-11-27 19:52:11 +0000

    rm comments

--- href_to_text.py
+++ href_to_text.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 
 href_data = pd.read_pickle("data\\href_data.pkl")
 href_data["Doc"] = ""
-# print(href_data['Href'][200])
+
 URL = "https://www.ecb.europa.eu"
 
 for index, references in enumerate(href_data['Href']):
@@ -22,13 +22,3 @@ for index, references in enumerate(href_data['Href']):
     href_data["Doc"][index] = doc
 
 href_data.to_pickle("data\\press_data.pkl")
-
-# content = requests.get(URL + href_data['Href'][200]).content
-# main = BeautifulSoup(content, "html.parser").find('main')
-#
-# section = main.find('div', attrs={'class': 'section'})
-# texts = section.findAll('p')
-#
-# doc = ""
-# for text in texts:
-#     doc += text.text
