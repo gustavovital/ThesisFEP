c2c26010530fdf1a92755c4cc6f65221467964a4
Gustavo de Oliveira <gustavovital@id.uff.br>
2021-12-08 00:51:04 +0000

    add tokenize script

--- tokenize.py
+++ tokenize.py
@@ -4,3 +4,22 @@
 # Data: 07/12/2021
 
 # Modules required
+import pandas as pd
+import nltk
+from nltk import ngrams
+
+# dataset
+press_data = pd.read_pickle('data\\press_data.pkl')
+
+# n-grams
+n = 2
+bigrams = ngrams(press_data['Doc'][0].split(), n)
+
+for grams in bigrams:
+    print(grams)
+
+# -----------------------------------------------------------------------------
+
+tokens = nltk.word_tokenize(press_data['Doc'][0].split())
+
+punkt
