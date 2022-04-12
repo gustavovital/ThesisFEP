cdd0fff231c21b04d20f0b3e48df312d09b418d5
Gustavo de Oliveira <gustavovital@id.uff.br>
2022-04-12 20:37:40 +0100

    add data_variable (the main dataframe)

--- codes/data_analysis.py
+++ codes/data_analysis.py
@@ -42,15 +42,107 @@ interest = pd.DataFrame({'date': gdp['date'], 'interest': interest.groupby(pd.Pe
 ppi = pd.DataFrame({'date': gdp['date'], 'ppi': ppi.groupby(pd.PeriodIndex(ppi['date'], freq="Q"))['ppi'].mean().reset_index()['ppi'][:-1]})
 une = pd.DataFrame({'date': gdp['date'], 'une': une.groupby(pd.PeriodIndex(une['date'], freq="Q"))['une'].mean().reset_index()['une'][:-1]})
 
-fig, axs = plt.subplots(3, 2)
-axs[0, 0].plot(gdp_diff['date'], gdp_diff['gdp'])
-axs[0, 0].set_title('')
-axs[0, 1].plot(gdp['date'], gdp['gdp'])
-axs[0, 1].set_title('Axis [0, 1]')
-axs[1, 0].plot(x, -y, 'tab:green')
-axs[1, 0].set_title('Axis [1, 0]')
-axs[1, 1].plot(x, -y, 'tab:red')
-axs[1, 1].set_title('Axis [1, 1]')
+# plots
+# fig, axs = plt.subplots(3, 2)
+# axs[0, 0].plot(gdp_diff['date'], gdp_diff['gdp'])
+# axs[0, 0].set_title('gdp_diff')
+# axs[0, 1].plot(gdp['date'], gdp['gdp'])
+# axs[0, 1].set_title('gdp')
+# axs[1, 0].plot(cpi['date'], cpi['cpi'])
+# axs[1, 0].set_title('cpi')
+# axs[1, 1].plot(interest['date'], interest['interest'])
+# axs[1, 1].set_title('interest')
+# axs[2, 0].plot(ppi['date'], ppi['ppi'])
+# axs[2, 0].set_title('ppi')
+# axs[2, 1].plot(une['date'], une['une'])
+# axs[2, 1].set_title('une')
+
+# Creating dataframe
+
+data = pd.DataFrame({'date':gdp['date'],
+                     'gdp_diff':gdp_diff['gdp'],
+                     'cpi':cpi['cpi'],
+                     'interest':interest['interest'],
+                     'ppi':ppi['ppi'],
+                     'une':une['une']})
+
+data = data[28:]  # From 2005
+lexicon = pd.read_csv('data\\data_lexicons_clean.csv')
+lexicon = lexicon.groupby(pd.PeriodIndex(lexicon['date'], freq='Q')).mean().reset_index()
+
+data.reset_index(inplace=True)
+data.drop(columns=['index'], inplace=True)
+
+data['lm_positive'] = lexicon.lm_positive
+data['lm_negative'] = lexicon.lm_negative
+data['vader_positive'] = lexicon.vader_positive
+data['vader_negative'] = lexicon.vader_negative
+data['words_count'] = lexicon.words_count
+data['recession'] = np.where(((data.date >= '2008-01-01') & (data.date <= '2009-06-30')) |
+         ((data.date >= '2011-07-01') & (data.date <= '2013-01-01')) |
+         ((data.date >= '2019-10-01') & (data.date <= '2020-06-30')), 1, 0)
 
 
+fig, axs = plt.subplots(2, 5)
+count = 1
+for i in range(0,2):
+    for j in range(0,5):
+        # print(count)
+        axs[i, j].plot(data['date'], data[data.columns[count]])
+        axs[i, j].set_title(data.columns[count])
+        count += 1
 
+# Unit root test
+
+from statsmodels.tsa.stattools import adfuller
+
+def adf(series):
+    for test in ['c', 'ct', 'ctt', 'nc']:
+        print('==================================================================')
+        print(f'Augmented Dickey-Fuller Test. \nConstant and trend order to include in regression: {test}\n')
+        dftest = adfuller(series, autolag='AIC', regression=test)
+        dfoutput = pd.Series(
+            dftest[0:4],
+            index=[
+                'Test Statistic',
+                'p-value',
+                'Lags Used',
+                'Number of Observations'
+            ],
+        )
+        for key, value in dftest[4].items():
+            dfoutput['Critical value (%s)' % key] = value
+        print(dfoutput)
+        print('==================================================================')
+        if dftest[0] <= dftest[4]['1%']:
+            print('Do not reject H0 at 1%')
+        elif dftest[0] <= dftest[4]['5%']:
+            print('Do not reject H0 at 5%')
+        elif dftest[0] <= dftest[4]['10%']:
+            print('Do not reject H0 at 10%')
+        else:
+            print('Reject H0')
+        print('==================================================================')
+
+# Check stationarity
+for column in data.drop(['words_count'], axis=1).columns[1:10]:
+    print('\n')
+    print('==================================================================')
+    print(f'VARIABLE TO TEST UNIT ROOT: {column}')
+    adf(data[column])
+
+
+data.to_csv('data\\data_variables.csv', index=False)
+'''Stationary variables of the dataframe data:
+
+The dataframe data is saved (above) as data_variables and it PROBABLY WILL BE CHANGED
+At  1%:     gdp_diff (c, ct, ctt, nc)
+            lm_positive (c, ct, ctt)  
+            vader_positive (c, ct, ctt)   
+    5%:     ppi (c)
+    10%:    interest (ct)
+            ppi (ctt)
+            lm_negative (c)
+
+Non stationary variables:   cpi, une, vader_negative
+'''
