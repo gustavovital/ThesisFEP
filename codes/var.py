af5f6d23af54c5a25d7f5d1f27a78c19235ef48c
Gustavo de Oliveira <gustavovital@id.uff.br>
2022-04-19 15:36:51 +0100

    start var and correlations

--- codes/var.py
+++ codes/var.py
@@ -1,3 +1,9 @@
+# Provide the var models for the dataset
+#
+# Gustavo Vital
+# Date: 19-04-2022
+
+# Modules to import
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -9,3 +15,33 @@ data = pd.read_csv('data\\data_variables.csv')
 
 data_var = data.set_index('date')
 data_var.dropna(inplace=True)
+
+# data_var.head()
+
+data_model = data[['gdp_diff', 'cpi_diff', 'ppi', 'interest_diff', 'une_diff', 'lm_positive']]
+data_model.dropna(inplace=True)
+# Testing models
+test_obs = 8
+train = data_model[:-test_obs]
+test = data_model[-test_obs:]
+
+for i in range(1, 6):  # VAR(1)
+    model = VAR(train)
+    results = model.fit(i)
+    print('Order =', i)
+    print('AIC: ', results.aic)
+    print('BIC: ', results.bic)
+    print()
+
+# SVAR
+result = model.fit(3)
+result.summary()
+
+# result.plot_acorr()
+# plt.show()
+
+irf = result.irf(10)
+
+irf.plot(orth=False, impulse='lm_positive')
+plt.show()
+
