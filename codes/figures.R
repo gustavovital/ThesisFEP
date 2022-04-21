06c8037806c0bee40216e41af6be567a1599754a
Gustavo de Oliveira <gustavovital@id.uff.br>
2022-04-21 09:25:56 +0100

    add first graph

--- codes/figures.R
+++ codes/figures.R
@@ -6,9 +6,52 @@ library(patchwork)
 # Add correlation plot ----
 data <- read_csv('data\\data_variables.csv')
 
-data %>%
+cpi_cps <- data %>%
   ggplot(aes(cpi, cps)) +
   geom_point() +
   geom_smooth(method = 'lm', se = FALSE, colour = 'gray50') +
-  labs(x=)
+  labs(x = 'Consumer Price Index',
+       y = 'Consumer Opinion Surveys: Consumer Prices') +
   theme_minimal()
+
+cpi_vader <- data %>%
+  ggplot(aes(cpi, vader_positive)) +
+  geom_point() +
+  geom_smooth(method = 'lm', se = FALSE, colour = 'gray50') +
+  labs(x = 'Consumer Price Index',
+       y = 'VADER Positive sentiment') +
+  theme_minimal()
+
+cpi_lm <- data %>%
+  ggplot(aes(cpi, lm_positive)) +
+  geom_point() +
+  geom_smooth(method = 'lm', se = FALSE, colour = 'gray50') +
+  labs(x = 'Consumer Price Index',
+       y = 'Loughran-McDonald Positive Sentiment') +
+  theme_minimal()
+
+vader_loughram <- data %>%
+  ggplot(aes(vader_positive, lm_positive)) +
+  geom_point() +
+  geom_smooth(method = 'lm', se = FALSE, colour = 'gray50') +
+  labs(x = 'VADER Positive sentiment',
+       y = 'Loughran-McDonald Positive Sentiment') +
+  theme_minimal()
+
+cps_vader <- data %>%
+  ggplot(aes(cps, vader_positive)) +
+  geom_point() +
+  geom_smooth(method = 'lm', se = FALSE, colour = 'gray50') +
+  labs(x = 'Consumer Price Index',
+       y = 'VADER Positive sentiment') +
+  theme_minimal()
+
+cps_lm <- data %>%
+  ggplot(aes(cps, lm_positive)) +
+  geom_point() +
+  geom_smooth(method = 'lm', se = FALSE, colour = 'gray50') +
+  labs(x = 'Consumer Opinion Surveys: Consumer Prices',
+       y = 'Loughran-McDonald Positive Sentiment') +
+  theme_minimal()
+
+(cpi_lm + cpi_vader + cpi_cps) / (cps_lm + cps_vader + vader_loughram)
