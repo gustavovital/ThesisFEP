10f318cae37b0a3173f26f9bbedb6d91319c681e
Gustavo de Oliveira <gustavovital@id.uff.br>
2022-04-28 18:01:23 +0100

    test for monthly series

--- codes/unit_root.py
+++ codes/unit_root.py
@@ -1,9 +1,10 @@
-
+# Unit root test
+#
+# Gustavo Vital
+# 28-04-2022
 
 # Modules
 from statsmodels.tsa.stattools import adfuller
-from statsmodels.tsa.api import VAR
-from statsmodels.tools.eval_measures import rmse, aic
 import pandas as pd
 
 # Dataset
@@ -12,7 +13,7 @@ data = pd.read_csv('data\\data_variables.csv')
 def adf(series):
     for test in ['c', 'ct', 'ctt', 'nc']:
         print('==================================================================')
-        print(f'Augmented Dickey-Fuller Test. \nConstant and trend order to include in regression: {test}\n')
+        print(f'Augmented Dickey-Fuller Test. \nH0: UNIT ROOT\nConstant and trend order to include in regression: {test}\n')
         dftest = adfuller(series.dropna(), autolag='AIC', regression=test)
         dfoutput = pd.Series(
             dftest[0:4],
@@ -28,13 +29,13 @@ def adf(series):
         print(dfoutput)
         print('==================================================================')
         if dftest[0] <= dftest[4]['1%']:
-            print('Do not reject H0 at 1%')
+            print('Reject H0 at 1%')
         elif dftest[0] <= dftest[4]['5%']:
-            print('Do not reject H0 at 5%')
+            print('Reject H0 at 5%')
         elif dftest[0] <= dftest[4]['10%']:
-            print('Do not reject H0 at 10%')
+            print('Reject H0 at 10%')
         else:
-            print('Reject H0')
+            print('Do not reject H0.')
         print('==================================================================')
 
 # Check stationarity
@@ -44,7 +45,7 @@ for column in data.drop(['words_count'], axis=1).columns[1:len(data.columns)]:
     print(f'VARIABLE TO TEST UNIT ROOT: {column}')
     adf(data[column])
 
-'''Stationary variables of the dataframe data:
+'''Stationary variables of the QUARTER dataframe data: 
 
 The dataframe data is saved (above) as data_variables.
 At  1%:     gdp_diff (c, ct, ctt, nc)
@@ -69,4 +70,26 @@ At  1%:     gdp_diff (c, ct, ctt, nc)
             cps (c)
 
 Non stationary variables:   cpi, une, vader_negative
+
+------------------------------------------------------------------------
+
+Stationary variables of the MONTHLY dataframe data: .
+
+At  1%:     lm_positive (c, ct, ctt)   
+            lm_negative (c, ct, ctt) 
+            vader_positive (c, ct, ctt)
+            vader_negative (c, ct, ctt)
+            gdp_diff (c, ct, ctt, nc)
+            cpi_diff (c, ct, ctt, nc)
+            interest_diff (c, ct, ctt, nc)
+            une_diff (nc),
+            cps_diff (c, ct, ctt, nc)
+            gdp_gap (c, ct, nc)
+    5%:     ppi (c, ct, nc)    
+            gdp_gap (ctt)
+    10%:    ppi (ctt)   
+            une_diff (c) 
+            cps (c)
+    
+Non stationary variables: cpi, interest, une, cps  
 '''
