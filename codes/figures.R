81f96efff8948ca6a77e314eb6b32f13f0483ba0
Gustavo de Oliveira <gustavovital@id.uff.br>
2022-05-09 23:02:54 +0100

    test var models

--- codes/figures.R
+++ codes/figures.R
@@ -3,14 +3,14 @@
 # Gustavo Vital
 
 # Packages
-install.packages(c('tidyverse', 'patchwork'))
+# install.packages(c('tidyverse', 'patchwork'))
 
 library(tidyverse)
 library(patchwork)
 library(zoo)
 
 # Add correlation plot ----
-data <- read_csv('data\\data_M_variables.csv')
+data <- read_csv('data\\data.csv')
 
 # cpi_cps <- data %>%
 #   ggplot(aes(cpi, cps)) +
@@ -70,24 +70,24 @@ normalize <- function(x){
   (x - min(x, na.rm = TRUE))/(max(x, na.rm = TRUE) - min(x, na.rm = TRUE))
 }
 
-acum <- function(serie) {
+# acum <- function(serie) {
+#
+#   acum <- c()
+#   acum[1] <- serie[1]
+#
+#   for(indice in 2:length(serie)) {
+#     acum[indice] <- (acum[indice - 1])*(1 + serie[indice]/100)
+#   }
+#   acum <- (acum - 1)
+#   return(acum)
+# }
 
-  acum <- c()
-  acum[1] <- serie[1]
+# plot(data$date, rollmean(data$vader_positive, k = 12), type='l')
 
-  for(indice in 2:length(serie)) {
-    acum[indice] <- (acum[indice - 1])*(1 + serie[indice]/100)
-  }
-  acum <- (acum - 1)
-  return(acum)
-}
-
-plot(data$date, rollmean(data$vader_positive, k = 12), type='l')
-
-data %>%
-  ggplot(aes(x = normalize(cps))) +
-  geom_line(aes(y = normalize(vader_positive)), size = .3, colour = 'dodgerblue3')
-  # geom_line(aes(y = normalize(cps)), size = .3, colour = 'tomato2')
+# data %>%
+#   ggplot(aes(x = normalize(cps))) +
+#   geom_line(aes(y = normalize(vader_positive)), size = .3, colour = 'dodgerblue3')
+#   # geom_line(aes(y = normalize(cps)), size = .3, colour = 'tomato2')
 
 data %>%
   filter(date > '2010-01-01' & date < '2021-01-01') %>%
@@ -105,8 +105,6 @@ data %>%
   geom_smooth(aes(y = normalize(cps)), colour = 'tomato4', size = .3, alpha=.6, se = FALSE)
 
 
-plot(normalize())
-
 data.teste <-
   data.frame('indice' = rollmean((data$lm_positive + data$vader_positive)/2,
                                  k = 12, fill = NA, align = 'right'),
@@ -115,12 +113,26 @@ data.teste <-
              'cpi' = data$cpi, 'gdp' = data$gdp_gap,
              'interest' = data$interest)
 
-data.teste %>%
-  filter(date >= '2010-01-01' & date < '2021-01-01') %>%
-  ggplot(aes(x = date, y = indice)) +
-  geom_line() +
-  geom_smooth(method = 'lm')
+data %>%
+  # filter(date >= '2012-04-01' & date < '2017-12-01') %>%
+  ggplot(aes(x = date)) +
+  geom_line(aes(y = normalize(rollmean(vader_positive, k=12, fill = NA, align = 'right'))),
+            colour='tomato4') +
+  geom_line(aes(y = normalize(rollmean(lm_positive, k=12, fill = NA, align = 'right'))),
+            colour='tomato2') +
+  geom_line(aes(y = normalize(rollmean((vader_positive+lm_positive)/2, k=12, fill = NA, align = 'right'))),
+            colour='dodgerblue3') +
+  geom_line(aes(y = normalize(cps)))
 
+data %>%
+  # filter(date >= '2012-04-01' & date < '2017-12-01') %>%
+  ggplot(aes(x = date)) +
+  geom_line(aes(y = normalize(rollmean(vader_positive, k=12, fill=NA)))) +
+  geom_line(aes(y = normalize(cps)), colour='red')
+
+
+data %>%
+  ggplot(aes(x = ))
 
 summary(lm(cps ~ indice + cpi, data = data.teste))
 
@@ -128,3 +140,79 @@ summary(lm(cps ~ indice + cpi, data = data.teste))
 #  [7] "cps"            "gdp_gap"        "lm_positive"    "lm_negative"    "vader_positive" "vader_negative"
 # [13] "words_count"    "recession"      "cpi_diff"       "interest_diff"  "cps_diff"       "une_diff"
 # [19] "subprime"       "covid"
+
+
+
+
+
+plot(data$lm_negative, data$cps)
+
+
+
+
+summary(lm(cps ~ I(vader_negative*lm_negative/2), data))
+summary(lm(cps ~ vader_negative , data))
+summary(lm(cps ~ lm_negative, data))
+summary(lm(cps ~ lm_negative + vader_negative, data))  # not so good
+
+summary(lm(cps ~ I(I(rollmean(lm_negative, k=12, fill=NA, align = 'right'))*
+                     I(rollmean(vader_negative, k=12, fill=NA, align = 'right'))/2) + cpi, data))
+summary(lm(cps ~ I(rollmean(vader_negative, k=12, fill=NA, align = 'right')), data))
+summary(lm(cps ~ I(rollmean(lm_negative, k=12, fill=NA, align = 'right')), data))
+summary(lm(cps ~ I(rollmean(lm_negative, k=12, fill=NA, align = 'right')) + I(rollmean(vader_negative, k=12, fill=NA, align = 'right')), data))
+
+data %>%
+  ggplot(aes(x = cps, y = lm_negative)) +
+  geom_point() +
+  geom_smooth(method = 'lm')
+
+data %>%
+  # filter(date >= '2012-04-01' & date < '2017-12-01') %>%
+  ggplot(aes(x = date)) +
+  # geom_line(aes(y = normalize(rollmean(lm_negative, k=12, fill=NA, align = 'right'))), colour = 'tomato4') +
+  # geom_line(aes(y = normalize(rollmean(vader_negative, k=12, fill=NA, align = 'right'))), colour = 'tomato2') +
+  geom_line(aes(y = normalize(rollmean((lm_negative + vader_negative)/2, k=12, fill=NA, align = 'right')))) +
+  geom_line(aes(y = normalize(cps))) +
+  scale_x_date(limits = as.Date(c('2013-04-01', '2017-12-01')))
+
+
+# teste VAR
+library(vars)
+
+
+var_data = data.frame('indice' = data$lm_negative[2:nrow(data)],
+                      'cpi' = diff(data$cpi),
+                      'gdp' = data$gdp_cycle[2:nrow(data)],
+                      'interest' = diff(data$interest),
+                      'une' = diff(data$une)) %>%
+  drop_na()  # best model until now
+
+
+
+
+VARselect(var_data, lag.max = 2, type = "const")
+
+
+var_model <- VAR(var_data, p=2, type='const')
+#summary(var_model)
+
+
+plot(irf(var_model,
+           impulse = 'indice',
+           response = 'cpi',
+           n.ahead = 12,
+           boot = T,
+           ci = 0.60)
+)
+
+# plot(feir)
+
+# plot(var_data)
+
+plot(data$cpi, type='l')
+
+
+
+
+
+
