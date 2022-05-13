85873adb7641bd2abc9c2474ef732a7a57e5fb83
Gustavo de Oliveira <gustavovital@id.uff.br>
2022-05-13 22:40:19 +0100

    add lasso ridge elasticnet

---
+++ codes/penalized_regression.R
@@ -0,0 +1,11 @@
+# script providing penalized regressions:
+# 
+# LASSO, RIDGE, ELASTIC NET
+# 
+# Gustavo Vital
+
+# Packages
+
+# set wd ----
+setwd('C:/Users/gusta/Documents/mestrado/dissertation/thesis_code/')
+
