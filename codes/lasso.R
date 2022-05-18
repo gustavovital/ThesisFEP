55ce123a1b97b02a2fe322d87cec18a29782febb
Gustavo de Oliveira <gustavovital@id.uff.br>
2022-05-18 16:31:56 +0100

    finish lasso

--- codes/lasso.R
+++ codes/lasso.R
@@ -10,6 +10,7 @@ library(tidyverse)
 library(caret)
 library(Matrix)
 library(glmnet)
+library(Metrics)
 
 # set wd ----
 setwd('C:/Users/gusta/Documents/mestrado/dissertation/thesis_code/')
@@ -26,10 +27,12 @@ data.model <- data.frame('cps' = data$cps,
                          'cpi' = data$cpi,
                          'gdp' = data$gdp,
                          'ppi' = data$ppi,
-                         'index' = data$lm_negative,
+                         # 'index.lm' = data$lm_negative,
+                         # 'index.vader' = data$vader_negative,
                          'covid' = data$covid,
                          'debt_crises' = data$debt_crises,
-                         'index_12' = lag(data$lm_negative, 12),
+                         # 'index.lm_12' = lag(data$lm_negative, 12),
+                         # 'index.vader_12' = lag(data$vader_negative, 12),
                          'cps_12' = lag(data$cps, 12),
                          'une_12' = lag(data$une, 12),
                          'interest_12'= lag(data$interest, 12),
@@ -37,6 +40,16 @@ data.model <- data.frame('cps' = data$cps,
                          'gdp_12' = lag(data$gdp, 12),
                          'ppi_12' = lag(data$ppi, 12)) %>%
   drop_na() # remove NA
+
+sink(file = "data\\lasso_output.txt")
+
+cat('******************************************************************************************\n')
+cat('INDEX CONTROL: VADER \n')
+cat('******************************************************************************************\n')
+
+data.model.vader <- data.frame(data.model, 
+                               'index.vader' = data$vader_negative[13:nrow(data)],
+                               'index.vader_12' = lag(data$vader_negative, 12)[13:nrow(data)])
 
 for(i in 1:6){
   # define variables
@@ -46,10 +59,10 @@ for(i in 1:6){
   set.seed(214)
   
   # divide data set into train and test
-  split <- sample(nrow(data.model), floor(0.8*nrow(data.model)))
+  split <- sample(nrow(data.model.vader), floor(0.8*nrow(data.model.vader)))
   
-  data.train <- data.model[split, ]
-  data.test <- data.model[-split, ]
+  data.train <- data.model.vader[split, ]
+  data.test <- data.model.vader[-split, ]
   
   # get y and x by data.model
   y_train <- data.train[ , i]
@@ -68,21 +81,96 @@ for(i in 1:6){
   cv.lasso <- cv.glmnet(x = train.sparse, 
                         y = y_train)
   
-  lambda <- cv.lasso$lambda.1se
+  lambda <- cv.lasso$lambda.min
   
+  predict.lasso <- predict(lasso, test.sparse, type = 'response', s = lambda)
   
   # define coefficients of the matrix
   coefs <- as.matrix(coef(cv.lasso))
   ix <- which(abs(coefs[, 1]) > 0)
   
-  print("=============================================")
-  print(paste0("Model Lasso ", i, " - Dependent Variable: ", variable))
-  print("")
-  print(paste0("Best lambda: ", lambda))
-  print("")
-  print("Variavel used on the model:")
+  cat("=============================================\n")
+  cat(paste0("Model Lasso ", i, " - Dependent Variable: ", variable), '\n')
+  cat(paste0("Best lambda: ", lambda), '\n')
+  cat("Variavel used on the model:\n")
   print(coefs[ix,1, drop=FALSE])
-  print("")
-
+  cat("=============================================\n")
+  cat("Model Accuracy\n")
+  cat("=============================================\n")
+  results <- data.frame('MAE' = mae(predict.lasso, data.test[, i]),
+                        'MSE' = mse(predict.lasso, data.test[, i]),
+                        'RMSE' = rmse(predict.lasso, data.test[, i]),
+                        'R2' = R2(predict.lasso, data.test[, i])[1])
+  
+  rownames(results) <- ''
+  print(results)
+  cat('\n- - - - - - - - - - - - - - - - - - - - - - -\n')
 
 }
+
+cat('******************************************************************************************\n')
+cat('INDEX CONTROL: LM-SA \n')
+cat('******************************************************************************************\n')
+
+data.model.lm <- data.frame(data.model, 
+                               'index.lm' = data$lm_negative[13:nrow(data)],
+                               'index.lm_12' = lag(data$lm_negative, 12)[13:nrow(data)])
+
+for(i in 1:6){
+  # define variables
+  variables <- c('cps', 'une', 'interest', 'cpi', 'gdp', 'ppi')
+  variable <- variables[i]
+  
+  set.seed(214)
+  
+  # divide data set into train and test
+  split <- sample(nrow(data.model.lm), floor(0.8*nrow(data.model.lm)))
+  
+  data.train <- data.model.lm[split, ]
+  data.test <- data.model.lm[-split, ]
+  
+  # get y and x by data.model
+  y_train <- data.train[ , i]
+  y_test <- data.test[ , i]
+  
+  X_train <- data.train[ , -i]
+  X_test <- data.test[ , -i]
+  
+  train.sparse <- sparse.model.matrix(~., X_train)
+  test.sparse <- sparse.model.matrix(~., X_test)
+  
+  # determine the best lambda ----
+  lasso <- glmnet(x = train.sparse, 
+                  y = y_train)
+  
+  cv.lasso <- cv.glmnet(x = train.sparse, 
+                        y = y_train)
+  
+  lambda <- cv.lasso$lambda.min
+  
+  predict.lasso <- predict(lasso, test.sparse, type = 'response', s = lambda)
+  
+  # define coefficients of the matrix
+  coefs <- as.matrix(coef(cv.lasso))
+  ix <- which(abs(coefs[, 1]) > 0)
+  
+  cat("=============================================\n")
+  cat(paste0("Model Lasso ", i, " - Dependent Variable: ", variable), '\n')
+  cat(paste0("Best lambda: ", lambda), '\n')
+  cat("Variavel used on the model:\n")
+  print(coefs[ix,1, drop=FALSE])
+  cat("=============================================\n")
+  cat("Model Accuracy\n")
+  cat("=============================================\n")
+  results <- data.frame('MAE' = mae(predict.lasso, data.test[, i]),
+                        'MSE' = mse(predict.lasso, data.test[, i]),
+                        'RMSE' = rmse(predict.lasso, data.test[, i]),
+                        'R2' = R2(predict.lasso, data.test[, i])[1])
+  
+  rownames(results) <- ''
+  print(results)
+  cat('\n- - - - - - - - - - - - - - - - - - - - - - -\n')
+  
+}
+
+sink(file = NULL)
